5418b0b1e11a1d18d587005f055fd06e44cb7da6
Evan Steinkerchner <esteinkerchner@gmail.com>
2022-06-11 11:11:21 -0400

    Updated labels and button texts

--- src/main/java/me/roundaround/enchantmentcompat/config/EnchantmentCompatConfig.java
+++ src/main/java/me/roundaround/enchantmentcompat/config/EnchantmentCompatConfig.java
@@ -7,9 +7,9 @@ import me.roundaround.roundalib.config.option.BooleanConfigOption;
 public class EnchantmentCompatConfig extends ModConfig {
   public final BooleanConfigOption MOD_ENABLED;
   public final BooleanConfigOption INFINITY_MENDING;
+  public final BooleanConfigOption MULTISHOT_PIERCING;
   public final BooleanConfigOption PROTECTION;
   public final BooleanConfigOption DAMAGE;
-  public final BooleanConfigOption MULTISHOT_PIERCING;
 
   public EnchantmentCompatConfig() {
     super(EnchantmentCompatMod.MOD_ID);
@@ -22,26 +22,26 @@ public class EnchantmentCompatConfig extends ModConfig {
 
     INFINITY_MENDING = registerConfigOption(
         BooleanConfigOption
-            .builder("infinityMending", "enchantmentcompat.infinity_mending.label")
+            .yesNoBuilder("infinityMending", "enchantmentcompat.infinity_mending.label")
             .setComment("Whether to allow infinity and mending to combine.")
+            .build());
+
+    MULTISHOT_PIERCING = registerConfigOption(
+        BooleanConfigOption
+            .yesNoBuilder("multishotPiercing", "enchantmentcompat.multishot_piercing.label")
+            .setComment("Whether to allow multishot and piercing to combine.")
             .build());
 
     PROTECTION = registerConfigOption(
         BooleanConfigOption
-            .builder("protection", "enchantmentcompat.protection.label")
+            .yesNoBuilder("protection", "enchantmentcompat.protection.label")
             .setComment("Whether to allow the different protections to combine.")
             .build());
 
     DAMAGE = registerConfigOption(
         BooleanConfigOption
-            .builder("damage", "enchantmentcompat.damage.label")
+            .yesNoBuilder("damage", "enchantmentcompat.damage.label")
             .setComment("Whether to allow the different damages to combine.")
-            .build());
-
-    MULTISHOT_PIERCING = registerConfigOption(
-        BooleanConfigOption
-            .builder("multishotPiercing", "enchantmentcompat.multishot_piercing.label")
-            .setComment("Whether to allow multishot and piercing to combine.")
             .build());
   }
 }
