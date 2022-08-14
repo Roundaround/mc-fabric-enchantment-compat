05195880e88d9f3d94e746761cdc931864b3b5a7
Evan Steinkerchner <esteinkerchner@gmail.com>
2022-08-14 11:35:25 -0400

    Update to roundalib 0.2.1

--- src/main/java/me/roundaround/enchantmentcompat/compat/modmenu/ModMenuImpl.java
+++ src/main/java/me/roundaround/enchantmentcompat/compat/modmenu/ModMenuImpl.java
@@ -2,12 +2,13 @@ package me.roundaround.enchantmentcompat.compat.modmenu;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+
 import me.roundaround.enchantmentcompat.EnchantmentCompatMod;
-import me.roundaround.roundalib.config.gui.ConfigScreen;
+import me.roundaround.roundalib.config.gui.screen.ConfigScreen;
 
 public class ModMenuImpl implements ModMenuApi {
-    @Override
-    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return (screen) -> new ConfigScreen(screen, EnchantmentCompatMod.CONFIG);
-    }
+  @Override
+  public ConfigScreenFactory<?> getModConfigScreenFactory() {
+    return (screen) -> new ConfigScreen(screen, EnchantmentCompatMod.CONFIG);
+  }
 }
